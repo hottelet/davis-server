e5b013fdea45e11d4a900bea17f0d2ca17eb8ddf
Michael Beemer <Michael.Beemer@dynatrace.com>
2017-05-19 17:04:41 -0400

    Added filter security and additional methods

--- src/lib/controllers/filters.js
+++ src/lib/controllers/filters.js
@@ -5,6 +5,7 @@ const _ = require("lodash");
 const FilterModel = require("../models/filter");
 
 const logger = require("../core/logger");
+const DError = require("../core/error");
 
 class Filters {
 
@@ -16,18 +17,37 @@ class Filters {
 
   static async update(user, id, filter) {
     const f = await FilterModel.findById(id);
-    // TODO security
+    if (!filter) {
+      logger.info({ user }, `Unable to find a filter with the ID ${id}.`);
+      throw new DError("Unable to find a filter with that ID!", 400);
+    } else if (!filter.owner.equals(user._id)) {
+      logger.warn({ user }, "Rejecting unauthorized filter deletions.");
+      throw new DError("Only the owner of the filter can update it.", 403);
+    }
     _.assign(f, filter);
     return f.save();
   }
 
   static async delete(user, id) {
-    // TODO security
-    return FilterModel.findByIdAndRemove(id);
+    const filter = FilterModel.findById(id);
+
+    if (!filter) {
+      logger.info({ user }, `Unable to find a filter with the ID ${id}.`);
+      throw new DError("Unable to find a filter with that ID!", 400);
+    } else if (!filter.owner.equals(user._id)) {
+      logger.warn({ user }, "Rejecting unauthorized filter deletions.");
+      throw new DError("Only the owner of the filter can delete it.", 403);
+    }
+
+    return FilterModel.remove(id);
   }
 
   static async getFilters(user) {
     return FilterModel.find({ tenant: user.tenant });
+  }
+
+  static async getById(id) {
+    return FilterModel.findById(id);
   }
 
   static async getFiltersByScope(user, scope) {
@@ -45,7 +65,7 @@ class Filters {
       .where("scope").in(scopeArr);
 
     const sortedFilters = _.sortBy(filters, f => -f.priority);
-    if (sortedFilters > 0) {
+    if (sortedFilters.length > 0) {
       return _.filter(sortedFilters, f => f.scope === sortedFilters[0].scope);
     }
     logger.debug("No matching filters found.");
